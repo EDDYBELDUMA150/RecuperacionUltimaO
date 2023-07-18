ba1dc3dad1013d0a476e68685dda07570176e06e
Eddy Belduma <eddy.belduma.est@tecazuay.edu.ec>
2023-07-18 16:18:30 -0500

    Comit inicial

---
+++ Downloads/backloginav/src/main/java/com/example/backloginav/BackloginavApplication.java
@@ -0,0 +1,13 @@
+package com.example.backloginav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackloginavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackloginavApplication.class, args);
+	}
+
+}
---
+++ Downloads/backloginav/src/test/java/com/example/backloginav/BackloginavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backloginav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackloginavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
